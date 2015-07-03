d0624631c43421d9427e1572866728a60e5647be
Janire <janire.diaz@gmail.com>
2015-07-03 10:33:26 +0200

    Algunos cambios en Nbody

--- hw1/NBody.java
+++ hw1/NBody.java
@@ -6,6 +6,31 @@ public class NBody{
 		String filename = args [2];
 
 		In in = new In(filename);
+		 	in.readInt();
+        	in.readDouble();
+
+	
+		while(in.hasnext() == true){
+		
+		}
+
+		for(i=0; i<dt; dt=dt + dt){
+			//Ahora como llamo a los metodos setNetForce, update y draw??
+		}
+
+		public static Planet getPlanet(In in){
+
+
+			double x = in.readDouble();
+			double y = in.readDouble();
+			double xVelocity = in.readDouble();
+			double yVelocity = in.readDouble();
+			double xAccel = in.readDouble();
+			double yAccel = in.readDouble();
+			String img = in.readString();
+
+
+		}
 
 
 	}
