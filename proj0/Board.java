ee5dc7774f11c3f19b131fc193fb09c26123aed3
Janire <janire.diaz@gmail.com>
2015-10-07 10:53:28 +0200

    New things in Board.java

--- proj0/Board.java
+++ proj0/Board.java
@@ -3,13 +3,40 @@ package checkers61b;
 public class Board{
 
 	private Piece[][] pieces;
+
+	public static void main(String[] args) {
+		int N = 8;
+		Board board = new Board(false);
+		StdDrawPlus.setXscale(0, N);
+		StdDrawPlus.setYscale(0, N);
+
+	}
 	
 
 	public Board(boolean shouldBeEmpty){
+		pieces = new Pieces[8][8];
+		if(!shouldBeEmpty){
+			boolean isFire = true;
+			for(int i = 0; i < 8; i++){
+				if (i >= 3){
+					isFire = false;
+				}
+
+				for(int j = 0; j < 8; j++){
+
+				}
+			}
+
+		}
 
 	}
 
 	public Piece pieceAt(int x, int y){
+		if(!inBounds(x, y)){
+			return null;
+		}
+
+		return pieces[x][y];
 
 	}
 
