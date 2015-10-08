83384e98d11065b3b87db0f429bbaee3759f7883
Janire <janire.diaz@gmail.com>
2015-10-08 10:53:54 +0200

    canSelect method

--- proj0/Board.java
+++ proj0/Board.java
@@ -3,12 +3,18 @@ package checkers61b;
 public class Board{
 
 	private Piece[][] pieces;
+	private static Coord selectedCoord;
+	private Piece selectedPiece;
+	private int selectedPieceX, selectedPieceY;
+	private boolean hasMoved = false;
 
 	public static void main(String[] args) {
 		int N = 8;
 		Board board = new Board(false);
 		StdDrawPlus.setXscale(0, N);
 		StdDrawPlus.setYscale(0, N);
+
+	
 
 	}
 	
@@ -23,6 +29,16 @@ public class Board{
 				}
 
 				for(int j = 0; j < 8; j++){
+					if(i <= 2 || i >= 5) && (i + j) % 2 == 0){
+						String type = "peon";
+						if(i == 2 || i == 5)
+							type = "bomba";
+						if(i == 1 || i == 6)
+							type = "escudo";
+
+						pieces[j][i] = new Piece(isFire, j, i, type);
+
+					}
 
 				}
 			}
@@ -41,6 +57,27 @@ public class Board{
 	}
 
 	public boolean canSelect(int x, int y){
+
+		if(!inBounds(x, y))
+			return false;
+
+		boolean result = false;
+		Piece piece = pieceAt(x, y);
+
 		
+		
+	}
+
+	public void select(int x, int y){
+		if(selectedPiece == null || (pieceAt(x, y) != null && !hasMoved)){
+			selectedPiece = pieceAt(x, y);
+			selectedPieceX = x;
+			selectedPieceY = y;
+		}else{
+			selectedPiece.move(x, y);
+			selectedPieceX = x;
+			selectedPieceY = y;
+			hasMoved = true;
+		}
 	}
 }
